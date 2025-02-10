8ad02d66171c9853e7efa3a2def6144630b6f4b2
Alex Tinoco <tinocoalex003@gmail.com>
2025-02-10 12:59:08 +0100

    add prettier

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,7 +2,7 @@
 
 export default function NotFound() {
     return (
-        <div>
+        <div className="p-[]">
             Not Found
         </div>
     );
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,6 +1,6 @@
 export default function Navbar() {
     return (
-        <div className="flex w-full bg-amber-700">
+        <div className=" w-full py-6 bg-gradient-to-r from-cyan-300 to-emerald-500 via-blue-600 text-black flex">
             Navbar
         </div>
     );
