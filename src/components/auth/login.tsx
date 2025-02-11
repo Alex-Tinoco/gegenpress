c1f0fd6a44bd7f3b746c1b034bc28c894441d82a
Alex Tinoco <tinocoalex003@gmail.com>
2025-02-11 14:15:31 +0100

    auth model and ts changes

--- src/components/auth/login.tsx
+++ src/components/auth/login.tsx
@@ -1,6 +1,27 @@
-import { EyeDropperIcon, EyeIcon } from "@heroicons/react/24/solid";
+"use client";
+import { EyeDropperIcon, EyeIcon, EyeSlashIcon } from "@heroicons/react/24/solid";
+import { MouseEventHandler, useState } from "react";
+
 
 export default function Login() {
+  const[passwordVisibility, setPasswordVisibility] = useState(true);
+  const[passwordType, setPasswordType] = useState("password");
+  
+
+  const togglePasswordVisibility = () => {
+    passwordVisibility ? (
+      setPasswordVisibility(false),
+      setPasswordType("text")
+    ) : (
+      setPasswordType("password"),
+      setPasswordVisibility(true)
+    )
+  };
+
+  const handleSubmit = (e: React.MouseEvent<HTMLButtonElement>) => {
+      e.preventDefault();
+  };
+
   return (
     <div className="flex h-screen w-full flex-col items-center justify-center gap-5">
       <h1 className="text-4xl font-bold">Get Started</h1>
@@ -23,23 +44,28 @@ export default function Login() {
       </div>
 
       <form className="flex w-1/3 flex-col gap-3">
-        <label className="translate-x-1 text-sm">Email</label>
+        <label className="translate-x-1 text-sm">Email <span className="text-red-600">*</span>
+        </label>
         <input
           type="email"
           placeholder="Enter your email"
-          className="w-full rounded-md border-1 border-gray-300 px-4 py-2 text-sm"
+          className="w-full rounded-md border-1 border-gray-300 p-2 text-sm"
+          required
         />
 
-        <label className="translate-x-1 text-sm">Password</label>
+        <label className="translate-x-1 text-sm">Password <span className="text-red-600">*</span></label>
         <div className="flex h-full flex-row items-center justify-center gap-1">
           <input
-            type="password"
+            type={passwordType}
             placeholder="Enter your password"
-            className="w-full rounded-md border-1 border-gray-300 px-4 py-2 text-sm focus:border-gray-100"
+            className="w-full rounded-md border-1 border-gray-300 p-2 text-sm focus:border-gray-100"
+            required
           />
-          <EyeIcon className="h-9 rounded-md border-1 border-gray-300 p-1 text-gray-300" />
+          <div onClick={togglePasswordVisibility}>
+            {passwordVisibility? <EyeIcon className="h-9 rounded-md border-1 border-gray-300 p-1 text-gray-300 hover:bg-gray-100 cursor-pointer" /> : <EyeSlashIcon className="h-9 rounded-md border-1 border-gray-300 p-1 text-gray-300 hover:bg-gray-100 cursor-pointer" />}
+          </div>
         </div>
-        <button className="flex w-full items-center justify-center gap-2 rounded-md border-2 bg-indigo-500 py-2 text-white hover:bg-indigo-600">
+        <button onClick={handleSubmit} className="flex w-full items-center justify-center gap-2 rounded-md border-2 bg-indigo-500 py-2 text-white hover:bg-indigo-600 cursor-pointer">
           Sign in or create account
         </button>
       </form>
