20086bbce0ed7358612bbbc2049c38e2acc4aad8
Alex Tinoco <tinocoalex003@gmail.com>
2025-03-13 10:32:04 +0100

    geosearch done

--- src/components/weather/geosearch.tsx
+++ src/components/weather/geosearch.tsx
@@ -9,12 +9,22 @@ export default function WeatherGeoSearch() {
   const [selectedLocation, setSelectedLocation] = useState<Location | null>(
     null,
   );
+  const [error, setError] = useState<string>("");
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+    if (query.length > 0) {
+    try {
     const data = await searchByText(query);
     setData(data);
     console.log(data);
+    } catch (err) {
+      setError("An error occurred while fetching data.");
+    }
+
+    } else {
+      setError("This field cannot be empty.");
+    }
   };
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
@@ -27,7 +37,8 @@ export default function WeatherGeoSearch() {
       setData([]);
       setSelectedLocation(null);
     }
-  }, [query]);
+      else { setError(""); }
+    }, [query]);
 
   const SelectLocation = (city: Location) => {
     setSelectedLocation(city);
@@ -39,9 +50,12 @@ export default function WeatherGeoSearch() {
         <input
           type="text"
           placeholder="City or country.."
-          className="rounded-md border-1 p-2"
+          className={`rounded-md border-1 p-2 ${error && ("border-red-500")}`}
           onChange={handleChange}
         />
+        {error && (
+          <p className="text-sm text-red-500">{error}</p>
+        )}
         <button
           type="submit"
           className="bg-main hover:bg-main-darker rounded-md p-2 text-white"
@@ -55,7 +69,7 @@ export default function WeatherGeoSearch() {
             <p
               key={`${element.lat}-${element.lng}`}
               onClick={() => SelectLocation(element)}
-              className={`w-full border-b border-gray-200 py-1 hover:bg-gray-200 ${selectedLocation?.lat === element.lat && selectedLocation?.lng === element.lng ? "bg-gray-300" : ""}`}
+              className={`w-full border-b border-gray-200 py-1 hover:bg-gray-200 pl-1 cursor-pointer ${selectedLocation?.lat === element.lat && selectedLocation?.lng === element.lng ? "bg-gray-300" : ""}`}
             >
               {element.name.charAt(0).toUpperCase() +
                 element.name.slice(1).toLowerCase()}
@@ -67,7 +81,7 @@ export default function WeatherGeoSearch() {
       )}
       {selectedLocation && (
         <div className="flex flex-row justify-between gap-2">
-          <button className="w-full rounded-md bg-green-500 p-2 text-white hover:bg-green-600">
+          <button className="w-full rounded-md bg-green-500 p-2 cursor-pointer text-white hover:bg-green-600">
             Select as my location
           </button>
         </div>
